cd59e0d162743c09ce5df19e06ee6f751d6bc050
fabioarduin <fabioarduin@gmail.com>
2021-11-10 11:47:17 -0300

    adiciona funcao de multiplicacao.

--- Calculadora.js
+++ Calculadora.js
@@ -12,9 +12,17 @@ function sub(){
 }
 
 function mult(){
-    //TODO: Implementar multiplicação
+    let num1 = 5;
+    let num2 = 10;
+    let mult = num1 * num2;
+    console.log(mult)
 }
 
 function div(){
     //TODO: Implementar divisão aqui
+    let num1 = 10;
+    let num2 = 5;
+    let sub = num1 / num2;
+    console.log(div)
 }
+
