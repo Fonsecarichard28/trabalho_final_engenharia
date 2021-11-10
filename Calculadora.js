7d3cf1a69547dc2d6a674745cc272f67187b8a40
richard fonseca <fonsecarichard28@gmail.com>
2021-11-10 10:37:58 -0300

    fix: additing function sum

--- Calculadora.js
+++ Calculadora.js
@@ -1,6 +1,11 @@
 function sum(){
-    //TODO: Implementar soma
+    let num1 = 5;
+    let num2 = 10;
+    let soma = num1 + num2;
+    console.log(soma)
 }
+
+console.log(sum())
 
 function sub(){
     //TODO: Implementar subtração
